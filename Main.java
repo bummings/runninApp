9eebebf41aea7e9a0b2a50bcae1ea5fc0d7e3cba
bummings <hello@bummings.io>
2023-05-07 13:51:41 -0400

    develops case statement logic further

--- Main.java
+++ Main.java
@@ -7,16 +7,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to run tracker, bruh ");
-        System.out.println("What do you want to do? \nInput ONE or TWO: ");
+        System.out.println("What do you want to do? \n1. Enter a run\n2. Enter shoes ");
 
         String userInput = scanner.nextLine().toUpperCase();
         
         String result = "";
         switch (userInput) {
-            case "ONE":
-                result = "You selected ONE";
+            case "1":
+                result = "How many miles did you run today?";
+                double milesRan = scanner.nextDouble();
+                System.out.println("OK bruh you ran " + milesRan + " miles.");
+                System.out.println("I am really proud of you wow man amazing");
+                
+                try (FileWriter fileWriter = new FileWriter("selection.txt", true)) {
+                    fileWriter.write(milesRan + System.lineSeparator());
+                } catch (IOException e) {
+                    System.out.println("big time problem bruh" + e.getMessage());
+                }
                 break;
-            case "TWO":
+            case "2":
                 result = "You selected TWO";
                 break;
             default:
