a2a1336868f99c6936a471c0ee2f301789397e65
bummings <hello@bummings.io>
2023-05-08 11:15:25 -0400

    comments for Main class

--- Main.java
+++ Main.java
@@ -1,20 +1,35 @@
+// The following imports are required: 
+//   Scanner for text input
+//    FileWriter for data persistance
+//    IOException for catching errors for data persistance
+
 import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        // Initialize scanner for input
         Scanner scanner = new Scanner(System.in);
         RunInput runInput = new RunInput();
 
+        // Utilize the RunInput class in Main class: this
+        // will handle the logic of accepting the miles
+        // ran, calculate the averages, and save them to
+        // a text file for data persistance.
         Double milesImport = runInput.handle(scanner);
         System.out.println("Your miles ran bruh: " + milesImport);
 
+        // This is the entry point for the run tracker.
         System.out.println("Welcome to run tracker, bruh ");
         System.out.println("What do you want to do? \n1. Enter a run\n2. Enter shoes ");
 
+        // Grab user input, capitalize the input to avoid
+        // any gotchas with lowercase input
         String userInput = scanner.nextLine().toUpperCase();
 
+        // Initialize an empty string for the menu
+        // selection.
         String result = "";
         switch (userInput) {
             case "1":
@@ -38,11 +53,17 @@ public class Main {
         }
         System.out.println(result);
 
+        // This try catch statement will attempt to write
+        // the result to a text file for data persistance
+        // and will throw an error if the attempt is
+        // unsuccessful.
         try (FileWriter fileWriter = new FileWriter("selection.txt", true)) {
             fileWriter.write(result + System.lineSeparator());
         } catch (IOException e) {
             System.out.println("big time problem bruh" + e.getMessage());
         }
+        // Closing the scanner to get rid of compiler
+        // warnings.
         scanner.close();
     }
 }
