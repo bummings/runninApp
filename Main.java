5a6c6d77f4bbec5299299209c47a9a3abe599382
bummings <hello@bummings.io>
2023-05-08 11:53:12 -0400

    configures gitignore to stop tracking output.txt

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 // The following imports are required: 
 //   Scanner for text input
-//    FileWriter for data persistance
-//    IOException for catching errors for data persistance
 
 import java.util.Scanner;
 import java.io.FileWriter;
@@ -11,14 +9,6 @@ public class Main {
     public static void main(String[] args) {
         // Initialize scanner for input
         Scanner scanner = new Scanner(System.in);
-        RunInput runInput = new RunInput();
-
-        // Utilize the RunInput class in Main class: this
-        // will handle the logic of accepting the miles
-        // ran, calculate the averages, and save them to
-        // a text file for data persistance.
-        Double milesImport = runInput.handle(scanner);
-        System.out.println("Your miles ran bruh: " + milesImport);
 
         // This is the entry point for the run tracker.
         System.out.println("Welcome to run tracker, bruh ");
@@ -33,16 +23,15 @@ public class Main {
         String result = "";
         switch (userInput) {
             case "1":
-                result = "How many miles did you run today?";
-                double milesRan = scanner.nextDouble();
-                System.out.println("OK bruh you ran " + milesRan + " miles.");
-                System.out.println("I am really proud of you wow man amazing");
+                // Utilize the RunInput class in Main
+                // class: this will handle the logic of
+                // accepting the miles ran, calculate the
+                // averages, and save them to a text file
+                // for data persistance.
+                RunInput runInput = new RunInput();
+                Double runInputMiles = runInput.handle(scanner);
+                System.out.println("Your miles ran bruh: " + runInputMiles);
 
-                try (FileWriter fileWriter = new FileWriter("selection.txt", true)) {
-                    fileWriter.write(milesRan + System.lineSeparator());
-                } catch (IOException e) {
-                    System.out.println("big time problem bruh" + e.getMessage());
-                }
                 break;
             case "2":
                 result = "You selected TWO";
--- RunInput.java
+++ RunInput.java
@@ -1,9 +1,26 @@
+//    FileWriter for data persistance
+//    IOException for catching errors for data persistance
+
 import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class RunInput {
     public Double handle(Scanner scanner) {
         System.out.println("Enter miles: ");
         Double milesInput = scanner.nextDouble();
+
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String timestamp = now.format(formatter);
+
+        try (FileWriter fileWriter = new FileWriter("output.txt", true)) {
+            fileWriter.write(timestamp + ": " + milesInput + System.lineSeparator());
+        } catch (IOException e) {
+            System.out.println("Problem encountered while trying to save to file: " + e.getMessage());
+        }
         return milesInput;
     }
 }
