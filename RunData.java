c2526f3395d3a386c02109e8e3d3ee1435bf68bc
bummings <hello@bummings.io>
2023-05-11 14:31:27 -0400

    updates RunData class with comments

--- RunData.java
+++ RunData.java
@@ -3,23 +3,29 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
 
 public class RunData {
     private List<RunEntry> entries;
 
     public RunData() {
-        // list of entries, each entry as a list
-        // with both date and time, separated.
+        // a list of entries, each entry as it's own list with a date, time, and shoes,
+        // all separated with a comma
+
+        // for reference: entries is an ArrayList of said runs with the three elements
+        // described abvove
         entries = new ArrayList<>();
+
+        // using BufferedReader to read from miles.txt
         try (BufferedReader br = new BufferedReader(new FileReader("miles.txt"))) {
+
             String line;
             // ensure that lines exist in file
             while ((line = br.readLine()) != null) {
-                // separate date from miles
+                // separate each distinct value within the entry
                 String[] parts = line.split(",");
                 if (parts.length > 2) {
                     LocalDate date = LocalDate.parse(parts[0], formatter);
@@ -35,32 +41,31 @@ public class RunData {
         }
     }
 
-    // declaring pattern for date format for parsing purposes
+    // defining pattern for date format for parsing
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    // Utilizes the getAverage method to figure out weekly average from the run
-    // entries.
+    // Utilizes the getAverage method to figure out weekly average of current and
+    // past run entries..
     public double getWeeklyAverage() {
         LocalDate today = LocalDate.now();
         LocalDate oneWeekAgo = today.minusWeeks(1);
         return getAverage(oneWeekAgo, today);
     }
 
-    // Utilizes the getAverage method to figure out weekly average from the run
-    // entries.
+    // Utilizes the getAverage method to figure out monthly average of current and
+    // past run entries.
     public double getMonthlyAverage() {
         LocalDate today = LocalDate.now();
         LocalDate oneMonthAgo = today.minusMonths(1);
         return getAverage(oneMonthAgo, today);
     }
 
-    // getAverage method will run thru the entries list one
-    // by one, adding them to totalMiles so long as they fit
-    // in the box of the beginning and end of the week/month.
     private double getAverage(LocalDate start, LocalDate end) {
         double totalMiles = 0;
         int count = 0;
 
+        // loop to determine if entries are within bounds for a weekly or monthly
+        // average:
         for (RunEntry entry : entries) {
             if (!entry.date.isBefore(start) && !entry.date.isAfter(end)) {
                 totalMiles += entry.miles;
@@ -71,8 +76,7 @@ public class RunData {
         return count > 0 ? totalMiles / count : 0;
     }
 
-    // Class describing the shape of each run within the
-    // entries list.
+    // Class describing the shape of each run entry within the entries list.
     private static class RunEntry {
         LocalDate date;
         double miles;
@@ -85,6 +89,9 @@ public class RunData {
         }
     }
 
+    // This is a map over all the shoes in shoes.txt file- using the getOrDefault
+    // method we can set a pair of shoes to 0.0 miles if it is not associated with
+    // any runs
     public Map<String, Double> getMilesByShoes() {
         Map<String, Double> milesByShoes = new HashMap<>();
         for (RunEntry run : entries) {
